570b06e26c7dab3e967200287e96de5bcc168414
lauren <153221621+lmcnaboe@users.noreply.github.com>
2024-12-12 19:29:44 -0500

    big butt

--- final project/index.js
+++ final project/index.js
@@ -508,3 +508,14 @@ document.getElementById('pisces').addEventListener('click', function() {
         pisces.addEventListener('click', () => {
         getRandomHoroscope('pisces');
       })
+
+      function keepBigButShowTXT() {
+        const horoBtn = document.getElementById("horoscope"); 
+        const horoscopeText = document.getElementById("title");
+        const horo = document.querySelector(".horo");
+        const horos = document.querySelector(".horos");
+        
+      horoscopeText.classList.add("active");
+
+      horoscopeText.style.display = "block";
+    }
