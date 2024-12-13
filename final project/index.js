bd9a133e5645654e5f4347718c5e7afd8db57f5d
lauren <153221621+lmcnaboe@users.noreply.github.com>
2024-12-12 19:10:13 -0500

    added more horoscopes and related stuff

--- final project/index.js
+++ final project/index.js
@@ -202,9 +202,6 @@
     Object.keys(themeVariables).forEach(property => {
         root.style.setProperty(property, themeVariables[property]);
     });
-
-    // Store selected theme in localStorage for persistence
-    localStorage.setItem('selectedTheme', theme);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -242,14 +239,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('pisces').addEventListener('click', function() {
     applyTheme('pisces')
     });
-
-
-function loadSelectedTheme() {
-    const selectedTheme = localStorage.getItem('selectedTheme');
-    if (selectedTheme) {
-        applyTheme(selectedTheme);
-    }
-    }
+});
 
     const horoscopes = {
         aries: [
@@ -341,60 +331,122 @@ function loadSelectedTheme() {
             "You know what you want and go for it",
             "You take challenges with grace",
             "You are an advocate of the world",
-            "You are a lost soul"
-            
+            "You are a lost soul",
+            "Details matter so ensure you research and clarify pertinent details for a work project.",
+            "If you’ve been dealing with a setback or delay then be prepared for it to suddenly come good.",
+            "If you’re planning on travelling this week, Virgo then double check routes and times.",
+            "Owning your own feelings is liberating, Virgo. Hiding your wants and dreams is a sure fire way to be disappointed with your lot.",
+            "If your relationship is plodding along then schedule some one on one time together where you don’t talk about work, the house or the kids.",
+            "You are soul searching but coming up empty. You don’t need to do this alone, ask friends and family to help you as they know you best and you’ll be surprised at their suggestions.",
+            "Focus on ways you can reconnect with your significant other, Virgo.",
+            "Your analytical tendencies have served you well, Virgo. They will keep you in demand in the workplace and keep your career progressing, however, using the same skills on people can be to your detriment.",
+            "You’ve made such good progress and are near to your goal, so don’t give up now. One last little push will get you over the finish line.",
+            "As a new week starts you will be craving to spend time with a new love interest or significant other."
         ],
         libra: [
             "You are adventurous",
             "You are going down the wrong path",
             "You will encounter a creature that frightens you",
             "You may have some feelings of numbness",
-            "You seek adventure"
-            
+            "You seek adventure",
+            "If your schedule is worrying you then consider cancelling a few social engagements.",
+            "Don’t let something that is bothering you fester. A heart to heart will clear the air and make things so much clearer.",
+            "If you’re on the market for a new partner then you may meet your match this week, Libra.",
+            "Research new ways to keep your health and fitness on track. You appear to be tiring of your current schedule and the rewards aren’t as good as you had hoped.",
+            "Make spending time with loved ones a priority this week. You will find yourself relaxing and laughing which you’ve not experienced much of recently.",
+            "No, life isn’t always fair but you should still seek justice and equality in the workplace.",
+            "At work, there is something in the pipeline for you, Libra. It will become clear towards the end of the week what it is.",
+            "Your self confidence is growing, Libra. This week is perfect to raise an emotional issue with a loved one or close friend.",
+            "There are some great changes ahead, Libra. One change is a relief of a financial burden, maybe a debt will be repaid or some unexpected money will find it’s way to you.",
+            "You have an urge to get to the bottom of a family matter and this is the week that you do."   
         ],
         scorpio: [
             "You will achieve your tasks",
             "You will have increased energy",
             "You will have to watch out for the people close to you",
             "You are motherly and care about others",
-            "You are a colorful person"
-            
+            "You are a colorful person",
+            "You may be used to hiding your feelings but this isn’t the time to hold back.",
+            "You may be feeling drawn to help your community in some way and you should definitely follow this through.",
+            "You have the right to stand your ground and be assertive and don’t let anyone tell you otherwise!",
+            "Try to stay centred in your own emotions when helping others navigate dark days.",
+            "Someone in authority needs to catch up with you. Don’t worry, it’s not a serious matter so stop avoiding it!",
+            "Someone has been greedy in wanting all of your attention and you need to put a stop to it.",
+            "It’s important to be able to communicate how you feel effectively, Scorpio.",
+            "Embrace your impulses, Scorpio. Never mind what others may think, change your hair if you want and buy that dress that you’re scared of being judged for – who cares!",
+            "You have faced many challenges in your life alone, Scorpio. It doesn’t have to be this way, reach out to those that care about you and share your worries and concerns.",
+            "This week, you will be inclined to prioritise fun, serotonin inducing behaviours. And why not?"            
         ],
         sag: [
             "Document your future",
             "You have a lot to see in your future",
             "You will receive fulfillment today",
             "You will see love all around you",
-            "You will find family away from yours"
-
+            "You will find family away from yours",
+            "Focus on the activities that bring you pleasure and do your best to carve out time to do it more.",
+            "You may be pushed into the spotlight at work this week and you should embrace it, not fear it.",
+            "You are hungry for something challenging and will consider leaving your current role.",
+            "You will be expressive and vivacious this week and others will see you in a different light.",
+            "You can shape your own destiny, within reason of course. Don’t just accept what you are given, go out into the world and take what you want.",
+            "Your energy is turned up to the max and you are lots of fun to be around.",
+            "Sometimes, being spontaneous turns out to be a lot of fun so go with the flow this week and accept invitations you would usually turn down.",
+            "You may feel your sense of security within a love match is waning, Saggi. As the week progresses, this will pass, and you will realise the opposite is in fact true.",
+            "With courage and determination you get can through most things, Saggi. However, you feel there’s an obstacle that you can’t quite get over, why is that?",
+            "You may be focussing on your living space this week and it is time to get the little odd jobs done and dusted before winter arrives."
           ],
           capricorn: [
             "You value family and bloodline",
             "You learn to deal with your stresses",
             "You will fight through your struggles",
             "You make great decisions",
-            "You are expressive and true to yourself"
-
+            "You are expressive and true to yourself",
+            "You’ve a busy schedule ahead and it will be an exhausting week. Make up for it at the weekend by ensuring you don’t fill up your diary with social engagements that you don’t want to attend anyway!",
+            "Money matters have been weighing on your mind and this week will bring some much needed clarity.",
+            "You have many good ideas and lots to contribute so what is stopping you? Speak up and get involved.",
+            "If you are feeling a little blue, then look at how you prioritise you own health and wellbeing.",
+            "You will make an impact this week and it will go in your favour when negotiating terms for a new role.",
+            "Looking on the bright side isn’t always the answer. You are smart enough to not brush things under the carpet and expect them to be forgotten.",
+            "A stimulating conversation will have you look at someone with fresh eyes – a possible love match, Capricorn?",
+            "You should trust where you are being guided spiritually this week, Capricorn.",
+            "You know you’ve been coasting at work and it needs to stop. You may think it’s not worth the effort but you’ve stagnated for far too long and can achieve so much more!"
           ],
           aquarius: [
             "You are great at making friends",
             "You get along with everyone",
             "You are beautiful",
             "You are hydrated",
-            "You are healthy and strong"
-
+            "You are healthy and strong",
+            "The early part of the week will bring a chance to catch up with friends and it is long overdue.",
+            "You may find yourself soul searching and looking for answers when the best thing would be to just get it straight from the horse’s mouth so to speak.",
+            "A family reunion is just around the corner and you couldn’t be less enthusiastic if you tried.",
+            "Look at your budget before committing to investments that make some time to realise.",
+            "Try to maintain your composure during a tricky period at work. Speaking out in frustration may come back to bite you so do keep the worst of it to yourself!",
+            "You’ve got a lot on your plate with work, errands, socialising etc. Getting stressed isn’t good so try to breakdown your diary into manageable portions.",
+            "Seek advice on money making strategies and follow it carefully",
+            "You will feel drawn to a business proposal that you would never usually consider.",
+            "At work, putting your hand up to learn new skills will earn you brownie points.",
+            "A collaboration with a colleague will propel your career. Don’t dismiss an idea because you don’t feel that you have the necessary skills, take your project to someone who does and watch it fly."
           ],
           pisces: [
             "You are fulfilled with love",
             "You are fulfilled with work",
             "You are fulfilled with your surroundings",
             "You are fashionable",
-            "You are kind"
-
+            "You are kind",
+            "An item that you’ve put off selling has increased in value significantly. Now is surely the time to market it in as many places as possible.",
+            "One on one relationships will flourish this week. Whether that be friends, family or your lover, you will find yourself forming deeper connections.",
+            "You may find yourself making decisions based on appearances rather than emotion this week.",
+            "Your empathetic side is going to be put to use this week, Pisces. You will feel like you must help everyone who comes to you but you really don’t.",
+            "Your leadership abilities are strengthening and you should stray out of your comfort zone by applying for roles with more responsibility.",
+            "You love to be at home and will enjoy your own company this week, Pisces. That’s fine as long as you don’t hibernate for the winter!",
+            "Sometimes it is easier to write your feelings down rather than speak about them, Pisces.",
+            "You will be inclined to daydream more than usual this week, Pisces. The truth of the matter is that a lot of what you wish for can happen with a little hard work and commitment to the cause.",
+            "You feel like you have been betrayed, Pisces. The truth of the matter is that they had your best interests at heart and didn’t mean to hurt you.",
+            "If you feel like your heart is being taken for granted then you must do something about it. Speak up and make sure you are heard, otherwise nothing will change."
           ]
       };
 
-      function getRandomPrompt(sheetName) {
+      function getRandomHoroscope(sheetName) {
         const prompts = horoscopes[sheetName];
         if (!prompts || prompts.length === 0) {
           console.error(`No prompts found for sheet "${sheetName}".`);
@@ -408,7 +460,51 @@ function loadSelectedTheme() {
         promptElement.textContent = randomPrompt;
       }
 
-      const seriousPrompt = document.getElementById('seriousPrompt');
-        seriousPrompt.addEventListener('click', () => {
-        getRandomPrompt('serious');
-});
+      const aries = document.getElementById('aries');
+        aries.addEventListener('click', () => {
+        getRandomHoroscope('aries');
+      })
+      const taurus = document.getElementById('taurus');
+        taurus.addEventListener('click', () => {
+        getRandomHoroscope('taurus');
+      })
+      const gemini = document.getElementById('gemini');
+        gemini.addEventListener('click', () => {
+        getRandomHoroscope('gemini');
+      })
+      const cancer = document.getElementById('cancer');
+        cancer.addEventListener('click', () => {
+        getRandomHoroscope('cancer');
+      })
+      const leo = document.getElementById('leo');
+        leo.addEventListener('click', () => {
+        getRandomHoroscope('leo');
+      })
+      const virgo = document.getElementById('virgo');
+        virgo.addEventListener('click', () => {
+        getRandomHoroscope('virgo');
+      })
+      const libra = document.getElementById('libra');
+        libra.addEventListener('click', () => {
+        getRandomHoroscope('libra');
+      })
+      const scorpio = document.getElementById('scorpio');
+        scorpio.addEventListener('click', () => {
+        getRandomHoroscope('scorpio');
+      })
+      const sag = document.getElementById('sag');
+        sag.addEventListener('click', () => {
+        getRandomHoroscope('sag');
+      })
+      const capricorn = document.getElementById('capricorn');
+        capricorn.addEventListener('click', () => {
+        getRandomHoroscope('capricorn');
+      })
+      const aquarius = document.getElementById('aquarius');
+        aquarius.addEventListener('click', () => {
+        getRandomHoroscope('aquarius');
+      })
+      const pisces = document.getElementById('pisces');
+        pisces.addEventListener('click', () => {
+        getRandomHoroscope('pisces');
+      })
